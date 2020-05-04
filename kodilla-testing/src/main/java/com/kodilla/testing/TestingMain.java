db979d5cc14fcf60e581d219d2980fd074bd70f8
bgola29 <bgola29@gmail.com>
2020-05-04 17:04:52 +0200

    Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,22 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
 
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+    Calculator calculator = new Calculator();
+    int testingAdd = calculator.add(23,25);
+    int testingSubtract = calculator.subtract(45 , 15);
+    if(testingAdd == 48){
+        System.out.println("Test calculator is ok");
+        }else {
+        System.out.println("Calculator tes has error");
+    }
+    if(testingSubtract == 30){
+        System.out.println("Test two calculator is ok");
+    } else {
+        System.out.println("Calculator test two has error");
+    }
     }
 }
--- kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
+++
@@ -1,6 +0,0 @@
-package com.kodilla.testing.user;
-
-public class SimpleUser {
-    
-    
-}
