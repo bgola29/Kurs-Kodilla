37d75a6907154a1d897296ef24d94ff255c0de43
bgola29 <bgola29@gmail.com>
2020-05-05 16:34:49 +0200

    kodilla-testing

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -4,19 +4,19 @@ import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
 
-    public static void main(String[] args){
-    Calculator calculator = new Calculator();
-    int testingAdd = calculator.add(23,25);
-    int testingSubtract = calculator.subtract(45 , 15);
-    if(testingAdd == 48){
-        System.out.println("Test calculator is ok");
-        }else {
-        System.out.println("Calculator tes has error");
-    }
-    if(testingSubtract == 30){
-        System.out.println("Test two calculator is ok");
-    } else {
-        System.out.println("Calculator test two has error");
-    }
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int testingAdd = calculator.add(23, 25);
+        int testingSubtract = calculator.subtract(45, 15);
+        if (testingAdd == 48) {
+            System.out.println("Test calculator is ok");
+        } else {
+            System.out.println("Calculator tes has error");
+        }
+        if (testingSubtract == 30) {
+            System.out.println("Test two calculator is ok");
+        } else {
+            System.out.println("Calculator test two has error");
+        }
     }
 }
--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -4,11 +4,12 @@ package com.kodilla.testing.calculator;
 public class Calculator {
 
 
-        public int add(int a, int b){
+    public int add(int a, int b) {
         int result = a + b;
         return result;
     }
-        public int subtract (int a, int b){
+
+    public int subtract(int a, int b) {
 
         int result = a - b;
         return result;
