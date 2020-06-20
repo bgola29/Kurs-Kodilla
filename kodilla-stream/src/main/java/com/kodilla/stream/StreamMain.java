7104da40202e1c0901d07e4636b9f96b2aba47bf
bgola29 <bgola29@gmail.com>
2020-06-20 18:39:41 +0200

    com.kodilla.stream.immutable

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,15 +1,12 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifer.PoemBeautifier;
-import com.kodilla.stream.lambda.*;
+
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        System.out.println(poemBeautifier.beautify("Ala ma kota ", text -> " ABC " + text + " CBA "));
-        System.out.println(poemBeautifier.beautify("Ala ma kota ", text -> text.toUpperCase()));
-        System.out.println(poemBeautifier.beautify("Ala ma kota ", text -> text.toLowerCase()));
-        System.out.println(poemBeautifier.beautify("Ala ma kota " , text -> " &!# " + text + " &!@ " ));
+        System.out.println("Jest to pierwsze uruchomienie generatora w trybie stream");
+        NumbersGenerator.generateEven(25);
     }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/iterate/NumbersGenerator.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/iterate/NumbersGenerator.java
@@ -1,4 +1,15 @@
 package com.kodilla.stream.iterate;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
 public class NumbersGenerator {
+    public static void generateEven(int max) {
+        Stream.iterate(1, n -> n + 1)
+                .limit(max)
+                .filter(n -> n % 2 == 0)
+                .forEach(System.out::println);
+    }
 }
