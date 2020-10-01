025f3e40f47fb2e17bbc0dab5f8fd1f49c2581a0
bgola29 <bgola29@gmail.com>
2020-10-01 17:19:07 +0200

    kodilla-course

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food/OrderServiceProcess.java
+++
@@ -1,8 +0,0 @@
-package com.kodilla.good.patterns.challenges.food;
-
-public class OrderServiceProcess {
-
-   // public static void main (String [] args){
-      //  OrderService orderService = new OrderService();
-   // }
-}
